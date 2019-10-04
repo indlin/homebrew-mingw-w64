1de5949bcb6dc37403226d429928eb4489c5c6ce
serj <indlin@gmail.com>
2019-10-04 15:44:13 +0300

    Create mingw-w64.rb

---
+++ mingw-w64.rb
@@ -0,0 +1,179 @@
+class MingwW64 < Formula
+  desc "Minimalist GNU for Windows and GCC cross-compilers"
+  homepage "https://mingw-w64.org/"
+  url "https://downloads.sourceforge.net/project/mingw-w64/mingw-w64/mingw-w64-release/mingw-w64-v6.0.0.tar.bz2"
+  sha256 "805e11101e26d7897fce7d49cbb140d7bac15f3e085a91e0001e80b2adaf48f0"
+  revision 2
+
+  bottle do
+    sha256 "19ac33d4237407b87afdc1b44dffa480fa4f38c4d3d55a15f8de12312df272b7" => :mojave
+    sha256 "ff170daabb31a4c437b2e1e0f55148e986751b4f9bd0ddcbf5c4b5a2e19c72aa" => :high_sierra
+    sha256 "60ad80431b2219a5cd1227c71bfae41550257de36476a05be0b8b05a6da4bcf3" => :sierra
+  end
+
+  # Apple's makeinfo is old and has bugs
+  depends_on "texinfo" => :build
+
+  depends_on "gmp"
+  depends_on "isl"
+  depends_on "libmpc"
+  depends_on "mpfr"
+
+  resource "binutils" do
+    url "https://ftp.gnu.org/gnu/binutils/binutils-2.32.tar.xz"
+    mirror "https://ftpmirror.gnu.org/binutils/binutils-2.32.tar.xz"
+    sha256 "0ab6c55dd86a92ed561972ba15b9b70a8b9f75557f896446c82e8b36e473ee04"
+  end
+
+  resource "gcc" do
+    url "https://ftp.gnu.org/gnu/gcc/gcc-9.1.0/gcc-9.1.0.tar.gz"
+    mirror "https://ftpmirror.gnu.org/gcc/gcc-9.1.0/gcc-9.1.0.tar.xz"
+    sha256 "be303f7a8292982a35381489f5a9178603cbe9a4715ee4fa4a815d6bcd2b658d"
+  end
+
+  def target_archs
+    ["i686", "x86_64"].freeze
+  end
+
+  def install
+    target_archs.each do |arch|
+      arch_dir = "#{prefix}/toolchain-#{arch}"
+      target = "#{arch}-w64-mingw32"
+
+      resource("binutils").stage do
+        args = %W[
+          --target=#{target}
+          --with-sysroot=#{arch_dir}
+          --prefix=#{arch_dir}
+          --enable-targets=#{target}
+          --disable-multilib
+        ]
+        mkdir "build-#{arch}" do
+          system "../configure", *args
+          system "make"
+          system "make", "install"
+        end
+      end
+
+      # Put the newly built binutils into our PATH
+      ENV.prepend_path "PATH", "#{arch_dir}/bin"
+
+      mkdir "mingw-w64-headers/build-#{arch}" do
+        system "../configure", "--host=#{target}", "--prefix=#{arch_dir}/#{target}"
+        system "make"
+        system "make", "install"
+      end
+
+      # Create a mingw symlink, expected by GCC
+      ln_s "#{arch_dir}/#{target}", "#{arch_dir}/mingw"
+
+      # Build the GCC compiler
+      resource("gcc").stage buildpath/"gcc"
+      args = %W[
+        --target=#{target}
+        --with-sysroot=#{arch_dir}
+        --prefix=#{arch_dir}
+        --with-bugurl=https://github.com/Homebrew/homebrew-core/issues
+        --enable-languages=c,c++,fortran
+        --with-ld=#{arch_dir}/bin/#{target}-ld
+        --with-as=#{arch_dir}/bin/#{target}-as
+        --with-gmp=#{Formula["gmp"].opt_prefix}
+        --with-mpfr=#{Formula["mpfr"].opt_prefix}
+        --with-mpc=#{Formula["libmpc"].opt_prefix}
+        --with-isl=#{Formula["isl"].opt_prefix}
+        --disable-multilib
+        --enable-threads=posix
+        --disable-sjlj-exceptions 
+        --with-dwarf2
+      ]
+
+      mkdir "#{buildpath}/gcc/build-#{arch}" do
+        system "../configure", *args
+        system "make", "all-gcc"
+        system "make", "install-gcc"
+      end
+
+      # Build the mingw-w64 runtime
+      args = %W[
+        CC=#{target}-gcc
+        CXX=#{target}-g++
+        CPP=#{target}-cpp
+        --host=#{target}
+        --with-sysroot=#{arch_dir}/#{target}
+        --prefix=#{arch_dir}/#{target}
+      ]
+
+      if arch == "i686"
+        args << "--enable-lib32" << "--disable-lib64"
+      elsif arch == "x86_64"
+        args << "--disable-lib32" << "--enable-lib64"
+      end
+
+      mkdir "mingw-w64-crt/build-#{arch}" do
+        system "../configure", *args
+        system "make"
+        system "make", "install"
+      end
+
+      # Build the winpthreads library
+      # we need to build this prior to the
+      # GCC runtime libraries, to have `-lpthread`
+      # available, for `--enable-threads=posix`
+      args = %W[
+        CC=#{target}-gcc
+        CXX=#{target}-g++
+        CPP=#{target}-cpp
+        --host=#{target}
+        --with-sysroot=#{arch_dir}/#{target}
+        --prefix=#{arch_dir}/#{target}
+      ]
+      mkdir "mingw-w64-libraries/winpthreads/build-#{arch}" do
+        system "../configure", *args
+        system "make"
+        system "make", "install"
+      end
+
+      # Finish building GCC (runtime libraries)
+      chdir "#{buildpath}/gcc/build-#{arch}" do
+        system "make"
+        system "make", "install"
+      end
+
+      # Symlinks all binaries into place
+      mkdir_p bin
+      Dir["#{arch_dir}/bin/*"].each { |f| ln_s f, bin }
+    end
+  end
+
+  test do
+    (testpath/"hello.c").write <<~EOS
+      #include <stdio.h>
+      #include <windows.h>
+      int main() { puts("Hello world!");
+        MessageBox(NULL, TEXT("Hello GUI!"), TEXT("HelloMsg"), 0); return 0; }
+    EOS
+    (testpath/"hello.cc").write <<~EOS
+      #include <iostream>
+      int main() { std::cout << "Hello, world!" << std::endl; return 0; }
+    EOS
+    (testpath/"hello.f90").write <<~EOS
+      program hello ; print *, "Hello, world!" ; end program hello
+    EOS
+
+    ENV["LC_ALL"] = "C"
+    ENV.remove_macosxsdk
+    target_archs.each do |arch|
+      target = "#{arch}-w64-mingw32"
+      outarch = (arch == "i686") ? "i386" : "x86-64"
+
+      system "#{bin}/#{target}-gcc", "-o", "test.exe", "hello.c"
+      assert_match "file format pei-#{outarch}", shell_output("#{bin}/#{target}-objdump -a test.exe")
+
+      system "#{bin}/#{target}-g++", "-o", "test.exe", "hello.cc"
+      assert_match "file format pei-#{outarch}", shell_output("#{bin}/#{target}-objdump -a test.exe")
+
+      system "#{bin}/#{target}-gfortran", "-o", "test.exe", "hello.f90"
+      assert_match "file format pei-#{outarch}", shell_output("#{bin}/#{target}-objdump -a test.exe")
+    end
+  end
+end
